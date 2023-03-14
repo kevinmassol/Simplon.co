642ec4bc93c48cf62e46a29765c8c2e5363e8b34
Kevin Massol <kevin.massol@icloud.com>
2023-03-14 17:58:12 +0100

    Initial Commit

---
+++ Simplon.co/Simplon_coApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simplon_coApp.swift
+//  Simplon.co
+//
+//  Created by KEVIN on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Simplon_coApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
