cd540136d4ab35e87b01d97ae59e9536229bd970
Kevin Massol <kevin.massol@icloud.com>
2023-03-17 10:11:53 +0100

    [feat #12] : add exercise 12

---
+++ Simplon.co/Exercise12/Exercise12.swift
@@ -0,0 +1,32 @@
+//
+//  Exercise12.swift
+//  Simplon.co
+//
+//  Created by KEVIN on 17/03/2023.
+//
+
+import SwiftUI
+
+struct Exercise12: View {
+    var body: some View {
+        VStack {
+            Text("SF Symbol weather list")
+                .font(.largeTitle)
+                .padding()
+            SymbolView(symbolName: "sun.min.fill", color: .blue)
+            SymbolView(symbolName: "sun.max.fill", color: .gray)
+            SymbolView(symbolName: "sunrise.fill", color: .green)
+            SymbolView(symbolName: "sunset.fill", color: .orange)
+            SymbolView(symbolName: "sun.dust.fill", color: .red)
+            SymbolView(symbolName: "sun.haze.fill", color: .purple)
+            SymbolView(symbolName: "cloud.fill", color: .mint)
+            SymbolView(symbolName: "cloud.drizzle.fill", color: .yellow)
+        }
+    }
+}
+
+struct Exercise12_Previews: PreviewProvider {
+    static var previews: some View {
+        Exercise12()
+    }
+}
---
+++ Simplon.co/Exercise12/SymbolView.swift
@@ -0,0 +1,29 @@
+//
+//  SymbolView.swift
+//  Simplon.co
+//
+//  Created by KEVIN on 17/03/2023.
+//
+
+import SwiftUI
+
+struct SymbolView: View {
+    
+    var symbolName: String
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: symbolName)
+                .foregroundColor(color)
+            Text(symbolName)
+                .foregroundColor(color)
+        }
+    }
+}
+
+struct SymbolView_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolView(symbolName: "sun.min.fill", color: .blue)
+    }
+}
